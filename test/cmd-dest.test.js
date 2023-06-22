ba6614523377090376dad6ef6cad32a3ecd6fe36
Ruben Padilla <35588894+rubenpad@users.noreply.github.com>
2023-06-22 14:34:25 -0500

    fix: use child process PID instead of parent PID for default name, and resolve issues with custom name and output destination (#148)
    * fix: use child process PID instead of parent PID for default name, and resolve issues with custom name and output destination
    
    * doc: update README

--- test/cmd-dest.test.js
+++ test/cmd-dest.test.js
@@ -5,45 +5,152 @@ const path = require('path')
 const { test } = require('tap')
 const ClinicHeapProfiler = require('../src/index.js')
 
-test('cmd - test collect - custom output destination', t => {
-  const tool = new ClinicHeapProfiler({ debug: true, dest: 'test-output-destination' })
+test('cmd - test collect - default name (child_process.pid) and output destination (.clinic)', t => {
+  const tool = new ClinicHeapProfiler({ debug: true })
 
-  function cleanup (err, filename) {
-    let count = 0
-
+  function cleanup (err, filepath) {
     function callback (err) {
       t.error(err)
-
-      if (++count === 2) {
-        t.end()
-      }
+      t.end()
     }
 
     t.error(err)
 
-    t.match(filename, /^test-output-destination$/)
+    t.match(filepath, /^\.clinic(\/|\\)[0-9]+\.clinic-heapprofiler$/)
 
-    fs.unlink(filename, callback)
-    fs.unlink(filename + '.html', callback)
+    fs.rm(path.dirname(filepath), { recursive: true }, callback)
   }
 
-  tool.collect([process.execPath, path.join('test', 'fixtures', 'randomHashes.js')], (err, filename) => {
+  tool.collect([process.execPath, path.join('test', 'fixtures', 'randomHashes.js')], (err, filepath) => {
     if (err) {
-      cleanup(err, filename)
+      cleanup(err, filepath)
       return
     }
 
-    t.ok(fs.statSync(filename).isFile())
+    t.match(filepath, tool.process.pid.toString())
 
-    tool.visualize(filename, `${filename}.html`, err => {
+    t.ok(fs.statSync(filepath).isFile())
+
+    tool.visualize(filepath, `${filepath}.html`, err => {
       if (err) {
-        cleanup(err, filename)
+        cleanup(err, filepath)
         return
       }
 
-      t.ok(fs.statSync(`${filename}.html`).isFile())
+      t.ok(fs.statSync(`${filepath}.html`).isFile())
 
-      cleanup(null, filename)
+      cleanup(null, filepath)
+    })
+  })
+})
+
+test('cmd - test collect - custom output destination', t => {
+  const tool = new ClinicHeapProfiler({ debug: true, dest: 'test-output-destination' })
+
+  function cleanup (err, filepath) {
+    function callback (err) {
+      t.error(err)
+      t.end()
+    }
+
+    t.error(err)
+
+    t.match(filepath, /^test-output-destination/)
+
+    fs.rm(path.dirname(filepath), { recursive: true }, callback)
+  }
+
+  tool.collect([process.execPath, path.join('test', 'fixtures', 'randomHashes.js')], (err, filepath) => {
+    if (err) {
+      cleanup(err, filepath)
+      return
+    }
+
+    t.ok(fs.statSync(filepath).isFile())
+
+    tool.visualize(filepath, `${filepath}.html`, err => {
+      if (err) {
+        cleanup(err, filepath)
+        return
+      }
+
+      t.ok(fs.statSync(`${filepath}.html`).isFile())
+
+      cleanup(null, filepath)
+    })
+  })
+})
+
+test('cmd - test collect - custom output name', t => {
+  const tool = new ClinicHeapProfiler({ debug: true, name: 'test-custom-name' })
+
+  function cleanup (err, filepath) {
+    function callback (err) {
+      t.error(err)
+      t.end()
+    }
+
+    t.error(err)
+
+    t.equal(filepath, '.clinic/test-custom-name.clinic-heapprofiler')
+
+    fs.rm(path.dirname(filepath), { recursive: true }, callback)
+  }
+
+  tool.collect([process.execPath, path.join('test', 'fixtures', 'randomHashes.js')], (err, filepath) => {
+    if (err) {
+      cleanup(err, filepath)
+      return
+    }
+
+    t.ok(fs.statSync(filepath).isFile())
+
+    tool.visualize(filepath, `${filepath}.html`, err => {
+      if (err) {
+        cleanup(err, filepath)
+        return
+      }
+
+      t.ok(fs.statSync(`${filepath}.html`).isFile())
+
+      cleanup(null, filepath)
+    })
+  })
+})
+
+test('cmd - test collect - custom name and output destination', t => {
+  const tool = new ClinicHeapProfiler({ debug: true, name: 'test-custom-name', dest: 'test-output-destination' })
+
+  function cleanup (err, filepath) {
+    function callback (err) {
+      t.error(err)
+      t.end()
+    }
+
+    t.error(err)
+
+    t.equal(filepath, 'test-output-destination/test-custom-name.clinic-heapprofiler')
+
+    fs.rm(path.dirname(filepath), { recursive: true }, callback)
+  }
+
+  tool.collect([process.execPath, path.join('test', 'fixtures', 'randomHashes.js')], (err, filepath) => {
+    if (err) {
+      cleanup(err, filepath)
+      return
+    }
+
+    t.ok(fs.statSync(filepath).isFile())
+
+    tool.visualize(filepath, `${filepath}.html`, err => {
+      if (err) {
+        cleanup(err, filepath)
+        return
+      }
+
+      t.ok(fs.statSync(`${filepath}.html`).isFile())
+
+      cleanup(null, filepath)
     })
   })
 })
